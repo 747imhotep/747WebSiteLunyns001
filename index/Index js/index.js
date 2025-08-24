3c47d15785881e59d92f7101e84b81c065c77d38
747imhotep <support@lunyns.com>
2025-08-24 20:18:35 +0200

    Update index html for JS File

--- index/Index js/index.js
+++ index/Index js/index.js
@@ -1,14 +1,5 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/68a8808ed6067019238eff16/1j393cbpd';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+console.log("Script loaded:", document.readyState);
 
-//     <script src="/index/Index js/index.js"> </script>
 
 // <!---------------------------------------------------------------------------------------->
 
@@ -162,3 +153,17 @@ document.getElementById("contactForm").addEventListener("submit", async function
   }
 });
 
+
+
+// <!---------------------------------------------------------------------------------------->
+//     Tawk.to Script START
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68a8808ed6067019238eff16/1j393cbpd';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+//     Tawk.to Script End
