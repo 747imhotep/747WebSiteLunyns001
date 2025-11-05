e0f155223eae004825bab4591a43a04eb0cf78bf
747imhotep <support@lunyns.com>
2025-11-05 14:09:37 +0100

    Update en59010.js

--- PRODUCTS/PROD_DET/EN590/en590 js/en59010.js
+++ PRODUCTS/PROD_DET/EN590/en590 js/en59010.js
@@ -23,6 +23,9 @@ document.getElementById("contactForm").addEventListener("submit", async function
 
         console.log("✅ Sender.net response:", result);
 
+        // clear form after successful submission
+        this.reset();
+
         // After Sender.net succeeds, submit Formspree
         this.submit();
 
@@ -36,35 +39,10 @@ document.getElementById("contactForm").addEventListener("submit", async function
 
 //⚠️ 
 
-//    You don’t prevent the default form submission, which is correct for Formspree. ✅
-//    However, this means the browser immediately navigates away to Formspree’s redirect after the form submits.
-//    That causes the fetch() to get aborted mid-request, exactly like your old NS_BINDING_ABORTED issue.
-//    So the Worker never fully completes the Sender.net submission.
+//    
+//    
+//    
+//    
 
 
 // <!---------------------------------------------------------------------------------------->
-// Cloudflare dashboard → Workers & Pages → sender-proxy-worker → Routes / Preview URL.
-
-// Name it something like sender-forwarder
-// Log in to Sender.net
-
-// Click on the Settings tab at the top.
-
-// Scroll to Variables → Add variable.
-
-// Paste your key in the Value field.
-
-// Save it. e5905ec3  178.51.79.47
-// name: John Doe
-// email: test@example.com
-// subject: Test
-// message: Hello world
-
-// body: JSON.stringify({
-//   email: email,
-//   first_name: name,
-//   list_ids: [178.51.79.47],          // <-- Replace 12345 with your actual list ID
-//   tags: ["quotation_form"],
-//   custom_fields: {
-//     subject: subject,
-//     message: message
